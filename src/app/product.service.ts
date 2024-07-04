1ee288f12e09f897d7565a56f9d55bfc80561dd0
Med Amine <med.amine.mzk@gmail.com>
2024-07-04 21:03:03 +0100

    Correction BUG + Hydra:member

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Product } from './Model/Product';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,26 +9,38 @@ import { Observable } from 'rxjs';
 export class ProductService {
 
   private apiUrl = 'http://localhost:8000/api/products';
+
+
+  private httpOptions = {
+    headers: new HttpHeaders({
+         'Content-Type': 'application/ld+json'
+    })
+  };
+
   constructor(private http: HttpClient) { }
 
   //retourne la liste des produits
-  getProducts(): Observable<Product[]> {
+  getProductsOld(): Observable<Product[]> {
     return this.http.get<Product[]>(this.apiUrl);
   }
+  getProducts(): Observable<Product[]> {
+    return this.http.get<{ 'hydra:member': Product[] }>(this.apiUrl)
+        .pipe(map(response => response['hydra:member']));
+}
   //retourne un produit par ID
   getProduct(id: number): Observable<Product> {
     return this.http.get<Product>(`${this.apiUrl}/${id}`);
   }
   //créer un produit
   createProduct(product: Product): Observable<Product> {
-    return this.http.post<Product>(this.apiUrl, product);
+    return this.http.post<Product>(this.apiUrl, product, this.httpOptions);
   }
-  //modifier un produit
+  //modifier Produit
   updateProduct(id: number, product: Product): Observable<Product> {
-    return this.http.put<Product>(`${this.apiUrl}/${id}`, product);
+      return this.http.put<Product>(`${this.apiUrl}/${id}`, product, this.httpOptions);
   }
-  //delete produit
+  //effacer produit
   deleteProduct(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+      return this.http.delete<void>(`${this.apiUrl}/${id}`, this.httpOptions);
   }
 }
